8faacb6f663e81659d74d1a32ad266a8a75ab718
Ety-Biswangri <etybgc02@gmail.com>
2022-05-02 18:51:04 +0600

    inventory data loaded

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ipcgk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const inventoriesCollection = client.db("fragnanceOfJoy").collection("inventories");
+
+        app.get('/inventories', async (req, res) => {
+            const query = req.query;
+            const cursor = inventoriesCollection.find(query);
+            const inventories = await cursor.toArray();
+
+            res.send(inventories);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+/* client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    console.log('DB Connected');
+}); */
+
 
 app.get('/', (req, res) => {
     res.send('Fragnance of Joy Server is Running');
