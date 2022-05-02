bda80f24b85d9892917e71b8f78f2b8aef9cfe2e
Ety-Biswangri <etybgc02@gmail.com>
2022-05-03 02:17:33 +0600

    get method finding one doc is implemented

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -24,6 +24,14 @@ async function run() {
             const inventories = await cursor.toArray();
 
             res.send(inventories);
+        })
+
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const inventory = await inventoriesCollection.findOne(query);
+
+            res.send(inventory);
         })
     }
     finally {
