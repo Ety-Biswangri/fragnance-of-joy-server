dc5530ea01f118775635a296c09913117f6da9f1
Ety-Biswangri <etybgc02@gmail.com>
2022-05-06 19:01:44 +0600

    product status updated

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ipcgk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -23,7 +39,7 @@ async function run() {
         app.post('/login', (req, res) => {
             const email = req.body;
             const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET);
-            console.log(token);
+            // console.log(token);
             res.send({ token });
         })
 
@@ -37,13 +53,20 @@ async function run() {
             res.send(inventories);
         })
 
-        app.get('/inventories', async (req, res) => {
+        app.get('/inventories', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.body.email;
-            const query = { email };
-            const cursor = inventoriesCollection.find(query);
-            const inventories = await cursor.toArray();
 
-            res.send(inventories);
+            if (email === decodedEmail) {
+                const query = { email };
+                const cursor = inventoriesCollection.find(query);
+                const items = await cursor.toArray();
+
+                res.send(items);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden access' });
+            }
         })
 
         app.get('/inventory/:id', async (req, res) => {
