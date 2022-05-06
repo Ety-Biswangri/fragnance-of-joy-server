cfde7b39add8cf2ba62da156d9c08abedc6fc111
Ety-Biswangri <etybgc02@gmail.com>
2022-05-07 01:10:35 +0600

    jwt is implemented

--- index.js
+++ index.js
@@ -9,21 +9,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
 function verifyJWT(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
+    const headersAuth = req.headers.authorization;
+    if (!headersAuth) {
         return res.status(401).send({ message: 'unauthorized access' });
     }
 
-    const token = authHeader.split(' ')[1];
+    const token = headersAuth.split(' ')[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
             return res.status(403).send({ message: 'forbidden access' });
         }
-        console.log('decoded', decoded);
         req.decoded = decoded;
         next();
     })
+
 }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ipcgk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -37,10 +38,12 @@ async function run() {
 
         // AUTHORIZATION
         app.post('/login', (req, res) => {
-            const email = req.body;
-            const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET);
+            const userEmail = req.body;
+            const accessToken = jwt.sign(userEmail, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
             // console.log(token);
-            res.send({ token });
+            res.send({ accessToken });
         })
 
 
@@ -53,7 +56,23 @@ async function run() {
             res.send(inventories);
         })
 
-        app.get('/inventories', verifyJWT, async (req, res) => {
+        app.get('/items', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+
+            if (email === decodedEmail) {
+                const query = { email };
+                const cursor = inventoriesCollection.find(query);
+                const items = await cursor.toArray();
+
+                res.send(items);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden access' });
+            }
+        })
+
+        /* app.get('/inventories', verifyJWT, async (req, res) => {
             const decodedEmail = req.decoded.email;
             const email = req.body.email;
 
@@ -67,7 +86,7 @@ async function run() {
             else {
                 res.status(403).send({ message: 'forbidden access' });
             }
-        })
+        }) */
 
         app.get('/inventory/:id', async (req, res) => {
             const id = req.params.id;
